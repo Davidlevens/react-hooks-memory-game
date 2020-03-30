713764b945ba111a75b4d0e8bfa81ccca42177bf
David Levens <52017470+Davidlevens@users.noreply.github.com>
2020-03-30 17:54:51 -0400

    Added levels.json, themes.json,game and components folder with Cell.jsx, GameField.jsx and Styled.js. Added game.reducer.js, index.jsx and game.utils.js to game folder. Changed App.js to App.jsx and modified.

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+import { path } from 'ramda';
+
+export function getFromTheme (themePath = '') {
+    return function getFromThemeProps (props = {}) {
+        return path(themePath.split('.'), props.theme);
+    }
+}
